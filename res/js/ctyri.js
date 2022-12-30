7d1a079a4b16769730f628e183105c66b341f3b1
FilipRiha <filipriha446@gmail.com>
2022-12-30 19:55:28 +0100

    konec

---
+++ res/js/ctyri.js
@@ -0,0 +1,32 @@
+const button = document.getElementById("button");
+const div = document.getElementById("div");
+const zivoty = document.getElementById("zivoty");
+const zivoty2 = document.getElementById("zivoty2");
+const info = document.getElementById("info");
+const level =document.getElementById("level");
+
+button.onmousedown = () => {
+  if(zivoty.innerHTML>0&&zivoty2.innerHTML>0){
+    div.style.left = "800px";
+    zivoty2.innerHTML--;
+  }
+  if (zivoty2.innerHTML <= 0) {
+    info.innerText = "zemřel";
+    
+    clearInterval(interval);
+  } 
+};
+
+
+button.onmouseup = () => {
+  div.style.left = "250px";
+};
+const interval = setInterval(()=>{
+zivoty.innerHTML--;
+if(zivoty.innerHTML<=0){
+    info.innerHTML="zemřel si"
+    info.style.backgroundImage="url(./res/img/pit_eror)";
+    clearInterval(interval);
+}
+
+},300)
